76f5eb2faad8702e08dc2ed75e1fbe37c2ec3dc6
sunil kumar <sunil.kumar@reddoorz.com>
2025-09-11 18:32:08 +0530

    update some packages

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         "snowflake-sqlalchemy>=1.4.5,<2.0.0",
         "numpy==1.26.4",
         "pandas>=1.5.3,<2.1.0",
+        "cryptography=*",
     ],
     extras_require={
         "test": [
